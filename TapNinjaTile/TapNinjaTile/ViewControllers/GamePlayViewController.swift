49ef5b2a713c819618166f2858732e01585788b4
otakisan <takashi@aquamarine.local>
2016-04-30 17:27:04 +0900

    アプリアイコンを設定

--- TapNinjaTile/TapNinjaTile/ViewControllers/GamePlayViewController.swift
+++ TapNinjaTile/TapNinjaTile/ViewControllers/GamePlayViewController.swift
@@ -46,8 +46,8 @@ class GamePlayViewController: UIViewController {
             
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            //skView.showsFPS = true
+            //skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
